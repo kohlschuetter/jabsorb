47aac4ace3866a751db7f0d9bf932d31bec46599
Christian Kohlschütter <christian@kohlschutter.com>
2023-10-04 14:08:34 +0200

    Improve BeanSerializer

--- src/main/java/org/jabsorb/serializer/impl/BeanSerializer.java
+++ src/main/java/org/jabsorb/serializer/impl/BeanSerializer.java
@@ -28,11 +28,15 @@ import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 
 import org.jabsorb.serializer.AbstractSerializer;
 import org.jabsorb.serializer.MarshallException;
@@ -51,19 +55,28 @@ public class BeanSerializer extends AbstractSerializer {
   /**
    * Stores the readable and writable properties for the Bean.
    */
-  protected static class BeanData {
+  private static class BeanData {
     // TODO: Legacy comment. WTF?
     // in absence of getters and setters, these fields are
     // public to allow subclasses to access.
     /**
      * The readable properties of the bean.
      */
-    public Map<String, Method> readableProps;
+    private final Map<String, MethodHandle> readableProps;
 
     /**
      * The writable properties of the bean.
      */
-    public Map<String, Method> writableProps;
+    private final Map<String, MethodHandle> writableProps;
+
+    private final Constructor<?> constructor;
+
+    private BeanData(Constructor<?> constructor, Map<String, MethodHandle> readableProps,
+        Map<String, MethodHandle> writableProps) {
+      this.constructor = constructor;
+      this.readableProps = readableProps;
+      this.writableProps = writableProps;
+    }
   }
 
   /**
@@ -101,30 +114,52 @@ public class BeanSerializer extends AbstractSerializer {
    * @param clazz The class of the bean to analyse
    * @return A populated BeanData
    * @throws IntrospectionException If a problem occurs during getting the bean info.
+   * @throws IllegalAccessException
+   * @throws NoSuchMethodException
    */
-  public static BeanData analyzeBean(Class<?> clazz) throws IntrospectionException {
+  public static BeanData analyzeBean(Class<?> clazz) throws IntrospectionException,
+      IllegalAccessException, NoSuchMethodException {
     log.info("analyzing " + clazz.getName());
-    BeanData bd = new BeanData();
-    bd.readableProps = new HashMap<String, Method>();
-    bd.writableProps = new HashMap<String, Method>();
-    if (clazz == Object.class) {
-      return bd;
+    Lookup lookup = MethodHandles.publicLookup();
+
+    Constructor<?> constructor;
+    try {
+      constructor = clazz.getDeclaredConstructor();
+      constructor.newInstance();
+    } catch (Exception e) {
+      constructor = null;
     }
+
     BeanInfo beanInfo = Introspector.getBeanInfo(clazz, Object.class);
     PropertyDescriptor props[] = beanInfo.getPropertyDescriptors();
+
+    Map<String, MethodHandle> readableProps = new HashMap<String, MethodHandle>();
+    Map<String, MethodHandle> writableProps = new HashMap<String, MethodHandle>();
+
     for (int i = 0; i < props.length; i++) {
       // This is declared by enums and shouldn't be shown.
       if (props[i].getName().equals("declaringClass")) {
+        // FIXME generalize, and check that this is an enum class
         continue;
       }
-      if (props[i].getWriteMethod() != null) {
-        bd.writableProps.put(props[i].getName(), props[i].getWriteMethod());
+      Method writeMethod = props[i].getWriteMethod();
+      if (writeMethod != null) {
+        Class<?> param[] = writeMethod.getParameterTypes();
+        if (param.length != 1) {
+          throw new IntrospectionException("bean " + clazz.getName() + " method " + writeMethod
+              .getName() + " does not have exactly one arg");
+        }
+
+        MethodHandle mh = lookup.unreflect(writeMethod);
+        writableProps.put(props[i].getName(), mh);
       }
-      if (props[i].getReadMethod() != null) {
-        bd.readableProps.put(props[i].getName(), props[i].getReadMethod());
+
+      Method readMethod = props[i].getReadMethod();
+      if (readMethod != null) {
+        readableProps.put(props[i].getName(), lookup.unreflect(readMethod));
       }
     }
-    return bd;
+    return new BeanData(constructor, readableProps, writableProps);
   }
 
   /**
@@ -139,7 +174,11 @@ public class BeanSerializer extends AbstractSerializer {
     synchronized (beanCache) {
       bd = beanCache.get(clazz);
       if (bd == null) {
-        bd = analyzeBean(clazz);
+        try {
+          bd = analyzeBean(clazz);
+        } catch (IllegalAccessException | NoSuchMethodException e) {
+          throw (IntrospectionException) new IntrospectionException(e.toString()).initCause(e);
+        }
         beanCache.put(clazz, bd);
       }
     }
@@ -173,22 +212,21 @@ public class BeanSerializer extends AbstractSerializer {
 
     JSONObject val = new JSONObject();
     marshallHints(val, o);
-    Object args[] = new Object[0];
     Object result;
-    for (Map.Entry<String, Method> ent : bd.readableProps.entrySet()) {
+    for (Map.Entry<String, MethodHandle> ent : bd.readableProps.entrySet()) {
       String prop = ent.getKey();
-      Method getMethod = ent.getValue();
+      MethodHandle getMethod = ent.getValue();
       if (log.isDebugEnabled()) {
-        log.debug("invoking " + getMethod.getName() + "()");
+        log.debug("invoking getter " + prop + " " + getMethod + "()");
       }
       try {
-        result = getMethod.invoke(o, args);
+        result = getMethod.invoke(o);
       } catch (Throwable e) {
         if (e instanceof InvocationTargetException) {
           e = ((InvocationTargetException) e).getTargetException();
         }
-        throw new MarshallException("bean " + o.getClass().getName() + " can't invoke " + getMethod
-            .getName() + ": " + e.getMessage(), e);
+        throw new MarshallException("bean " + o.getClass().getName() + " can't invoke getter:"
+            + prop + " " + getMethod + ": " + e.getMessage(), e);
       }
       try {
         if (result != null || ser.getMarshallNullAttributes()) {
@@ -220,8 +258,7 @@ public class BeanSerializer extends AbstractSerializer {
 
     int match = 0;
     int mismatch = 0;
-    for (Map.Entry<String, Method> ent : bd.writableProps.entrySet()) {
-      String prop = ent.getKey();
+    for (String prop : bd.writableProps.keySet()) {
       if (jso.has(prop)) {
         match++;
       } else {
@@ -239,18 +276,11 @@ public class BeanSerializer extends AbstractSerializer {
 
     ObjectMatch m = null;
     ObjectMatch tmp;
-    Iterator<String> i = jso.keys();
-    while (i.hasNext()) {
-      String field = i.next();
-      Method setMethod = bd.writableProps.get(field);
+    for (String field : jso.keySet()) {
+      MethodHandle setMethod = bd.writableProps.get(field);
       if (setMethod != null) {
         try {
-          Class<?> param[] = setMethod.getParameterTypes();
-          if (param.length != 1) {
-            throw new UnmarshallException("bean " + clazz.getName() + " method " + setMethod
-                .getName() + " does not have one arg");
-          }
-          tmp = ser.tryUnmarshall(state, param[0], jso.get(field));
+          tmp = ser.tryUnmarshall(state, setMethod.type().parameterType(1), jso.get(field));
           if (tmp != null) {
             if (m == null) {
               m = tmp;
@@ -288,26 +318,29 @@ public class BeanSerializer extends AbstractSerializer {
     if (log.isDebugEnabled()) {
       log.debug("instantiating " + clazz.getName());
     }
+
     Object instance;
     try {
-      instance = clazz.getDeclaredConstructor().newInstance();
+      instance = Objects.requireNonNull(bd.constructor).newInstance();
     } catch (InstantiationException | InvocationTargetException | IllegalAccessException
-        | NoSuchMethodException | RuntimeException e) {
+        | RuntimeException e) {
       throw new UnmarshallException("could not instantiate bean of type " + clazz.getName()
           + ", make sure it has a no argument " + "constructor and that it is not an interface or "
           + "abstract class", e);
     }
+
+    if (instance instanceof Throwable) {
+      // do not expose internals
+      ((Throwable) instance).setStackTrace(new StackTraceElement[0]);
+    }
+
     state.setSerialized(o, instance);
-    Object invokeArgs[] = new Object[1];
     Object fieldVal;
-    Iterator<String> i = jso.keys();
-    while (i.hasNext()) {
-      String field = i.next();
-      Method setMethod = bd.writableProps.get(field);
+    for (String field : jso.keySet()) {
+      MethodHandle setMethod = bd.writableProps.get(field);
       if (setMethod != null) {
         try {
-          Class<?> param[] = setMethod.getParameterTypes();
-          fieldVal = ser.unmarshall(state, param[0], jso.get(field));
+          fieldVal = ser.unmarshall(state, setMethod.type().parameterType(1), jso.get(field));
         } catch (UnmarshallException e) {
           throw new UnmarshallException("could not unmarshall field \"" + field + "\" of bean "
               + clazz.getName(), e);
@@ -316,17 +349,16 @@ public class BeanSerializer extends AbstractSerializer {
               + clazz.getName(), e);
         }
         if (log.isDebugEnabled()) {
-          log.debug("invoking " + setMethod.getName() + "(" + fieldVal + ")");
+          log.debug("invoking setter " + field + " " + setMethod + "(" + fieldVal + ")");
         }
-        invokeArgs[0] = fieldVal;
         try {
-          setMethod.invoke(instance, invokeArgs);
+          setMethod.invoke(instance, fieldVal);
         } catch (Throwable e) {
           if (e instanceof InvocationTargetException) {
             e = ((InvocationTargetException) e).getTargetException();
           }
-          throw new UnmarshallException("bean " + clazz.getName() + "can't invoke " + setMethod
-              .getName() + ": " + e.getMessage(), e);
+          throw new UnmarshallException("bean " + clazz.getName() + "can't invoke setter " + field
+              + " " + setMethod + ": " + e.getMessage(), e);
         }
       }
     }
